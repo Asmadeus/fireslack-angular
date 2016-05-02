463ea587bd3532259b94ebe8999fe35d82b668f7
Kirill Grevtsov <kgrevtsov@mail.ru>
2016-05-02 21:16:28 +0200

    firebase

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 angular
-  .module('angularfireSlackApp', [
+  .module('fireslack', [
     'firebase',
     'angular-md5',
     'ui.router'
@@ -22,13 +22,50 @@ angular
       })
       .state('login', {
         url: '/login',
-        templateUrl: 'auth/login.html'
+        templateUrl: 'auth/login.html',
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+          requireNoAuth: function ($state,Auth) {
+            return Auth.$requireAuth().then(function (auth) {
+              $state.go('home');
+            }, function (error) {
+              return;
+            });
+          }
+        }
       })
       .state('register', {
         url: '/register',
-        templateUrl: 'auth/register.html'
+        templateUrl: 'auth/register.html',
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+          requireNoAuth: function ($state,Auth) {
+            return Auth.$requireAuth().then(function (auth) {
+              $state.go('home');
+            }, function (error) {
+              return;
+            });
+          }
+        }
+      })
+      .state('profile',{
+        url: '/profile',
+        templateUrl: 'user/profile.html',
+        controller: 'ProfileCtrl as profileCtrl',
+        resolve: {
+          auth: function ($state,Users,Auth) {
+            return Auth.$requireAuth().catch(function () {
+              $state.go('home');
+            });
+          },
+          profile: function (Users,Auth) {
+            return Auth.requireAuth().then(function (auth) {
+              return Users.getProfile(auth.uid).$loaded();
+            });
+          }
+        }
       });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://slack.firebaseio.com/');
+  .constant('FirebaseUrl', 'https://fireslack-example.firebaseio.com/');
